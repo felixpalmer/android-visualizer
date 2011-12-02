80b5eeb678618b7a82a513da5d4cf480e6e1cde4
Felix Palmer <felix.palmer@metaswitch.com>
2011-12-02 13:23:17 -0800

    Added test mp3

--- src/com/pheelicks/visualizer/VisualizerActivity.java
+++ src/com/pheelicks/visualizer/VisualizerActivity.java
@@ -1,13 +1,19 @@
 package com.pheelicks.visualizer;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 public class VisualizerActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+  /** Called when the activity is first created. */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+
+    // Just play test file
+    MediaPlayer player = MediaPlayer.create(this, R.raw.test);
+    player.setLooping(true);
+    player.start();
+  }
 }
