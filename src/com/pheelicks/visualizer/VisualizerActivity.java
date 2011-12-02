a92a1757e7b882e0dbb00a7c580bfa3e09255083
Felix Palmer <felix.palmer@metaswitch.com>
2011-12-02 13:29:41 -0800

    Added stop/start buttons

--- src/com/pheelicks/visualizer/VisualizerActivity.java
+++ src/com/pheelicks/visualizer/VisualizerActivity.java
@@ -3,17 +3,28 @@ package com.pheelicks.visualizer;
 import android.app.Activity;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 
 public class VisualizerActivity extends Activity {
+  private MediaPlayer mPlayer;
+
   /** Called when the activity is first created. */
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
 
-    // Just play test file
-    MediaPlayer player = MediaPlayer.create(this, R.raw.test);
-    player.setLooping(true);
-    player.start();
+    mPlayer = MediaPlayer.create(this, R.raw.test);
+    mPlayer.setLooping(true);
+  }
+
+  public void startPressed(View view)
+  {
+    mPlayer.start();
+  }
+
+  public void stopPressed(View view)
+  {
+    mPlayer.stop();
   }
 }
