aa4df1cf60bee252c8343656e58b82c7508cbed9
Felix Palmer <felix.palmer@metaswitch.com>
2011-12-02 13:05:56 -0800

    Created Eclipse project

---
+++ src/com/pheelicks/visualizer/VisualizerActivity.java
@@ -0,0 +1,13 @@
+package com.pheelicks.visualizer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class VisualizerActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
